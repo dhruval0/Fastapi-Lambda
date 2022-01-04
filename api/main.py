7b6e674cbba7c7f71b6a21e9082e593b103f1e8c
dhruval0 <dhruvaldhorajia2014@gmail.com>
2022-01-04 17:49:15 +0530

    Removed workflow

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 def orchtel_home():
     return {"Welcome to": "lambda function"}
+
+handler = Mangum(app = app)
